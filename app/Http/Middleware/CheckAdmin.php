edd951589a8da05f6472a911330862190fc55e05
Arnaldo Manuel <70331507+arnaldomanuel@users.noreply.github.com>
2021-02-15 21:35:10 +0200

    commit

--- app/Http/Middleware/CheckAdmin.php
+++ app/Http/Middleware/CheckAdmin.php
@@ -17,7 +17,7 @@ class CheckAdmin
      */
     public function handle(Request $request, Closure $next)
     {
-        if (Auth::user()->admin != 1) {
+        if (Auth::user()->is_admin != 1) {
             return  redirect('/cursos');
          }
       
