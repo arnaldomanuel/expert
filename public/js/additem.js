b7297847ff50be656f47332f4402a5893480f49b
Arnaldo Manuel <70331507+arnaldomanuel@users.noreply.github.com>
2021-02-18 23:22:31 +0200

    Limitar caracteres n objectivo

--- public/js/additem.js
+++ public/js/additem.js
@@ -5,7 +5,7 @@ $(function() {
         $('#objectives').append(`  
             <div class="form-group">
                 <label for="objectiive${indexSocial}">Objectivo</label>
-                <input max="140" required type="text" class="form-control" name="objective${indexSocial}" id="objective${indexSocial}">
+                <input maxLength="140" required type="text" class="form-control" name="objective${indexSocial}" id="objective${indexSocial}">
             </div>
   `);
 
